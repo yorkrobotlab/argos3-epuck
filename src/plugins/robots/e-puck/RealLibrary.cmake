0da1c01d7314a1f5b2294d5baf5da13854315919
Russell Joyce <russell.joyce@york.ac.uk>
2020-05-11 19:45:31 +0100

    Removed some parts of the build that aren't needed and cause issues
    This includes:
    - All 'testing' targets
    - real_epuck_virtual_camrab_actuator
    - real_epuck_virtual_camrab_sensor
    - real_epuck_omnidirectional_camera_sensor
    - All 'real_robot/vision' targets
    - The epuck_camera_configurator_server executable

--- src/plugins/robots/e-puck/RealLibrary.cmake
+++ src/plugins/robots/e-puck/RealLibrary.cmake
@@ -19,30 +19,30 @@ set(ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT
   real_robot/real_epuck_range_and_bearing_sensor.h
   real_robot/real_epuck_ircom_sensor.h
   real_robot/real_epuck_micro_sensor.h
-  real_robot/real_epuck_omnidirectional_camera_sensor.h
-  real_robot/real_epuck_virtual_camrab_sensor.h
+  #  real_robot/real_epuck_omnidirectional_camera_sensor.h
+  #  real_robot/real_epuck_virtual_camrab_sensor.h
   # Actuators
   real_robot/real_epuck_serial_actuator.h
   real_robot/real_epuck_wheels_actuator.h
   real_robot/real_epuck_base_leds_actuator.h
   real_robot/real_epuck_rgb_leds_actuator.h
   real_robot/real_epuck_range_and_bearing_actuator.h
-  real_robot/real_epuck_ircom_actuator.h
-  real_robot/real_epuck_virtual_camrab_actuator.h)
+  real_robot/real_epuck_ircom_actuator.h)
+  #  real_robot/real_epuck_virtual_camrab_actuator.h)
 # Install location
 install(
   FILES ${ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT}
   DESTINATION include/argos3/plugins/robots/e-puck/real_robot)
 # Vision headers
-set(ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION
-  real_robot/vision/blob_detector.h
-  real_robot/vision/client_server_protocol.h
-  real_robot/vision/epuck_camera_configurator.h
-  real_robot/vision/epuck_image_grabber.h
-  real_robot/vision/extended_configurable_resource.h
-  real_robot/vision/image.h
-  real_robot/vision/image_grabber.h
-  real_robot/vision/distance_converter.h)
+#set(ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION
+	#  real_robot/vision/blob_detector.h
+	#  real_robot/vision/client_server_protocol.h
+	#  real_robot/vision/epuck_camera_configurator.h
+	#  real_robot/vision/epuck_image_grabber.h
+	#  real_robot/vision/extended_configurable_resource.h
+	#  real_robot/vision/image.h
+	#  real_robot/vision/image_grabber.h
+	#  real_robot/vision/distance_converter.h)
 # Install location
 install(
   FILES ${ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION}
@@ -52,7 +52,7 @@ install(
 set(ARGOS3_SOURCES_PLUGINS_ROBOTS_EPUCK
   ${ARGOS3_SOURCES_PLUGINS_ROBOTS_EPUCK}
   ${ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT}
-  ${ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION}
+  #  ${ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION}
   # Base
   real_robot/real_epuck.cpp
   real_robot/real_epuck_main.cpp
@@ -67,20 +67,20 @@ set(ARGOS3_SOURCES_PLUGINS_ROBOTS_EPUCK
   real_robot/real_epuck_range_and_bearing_sensor.cpp
   real_robot/real_epuck_ircom_sensor.cpp
   real_robot/real_epuck_micro_sensor.cpp
-  real_robot/real_epuck_omnidirectional_camera_sensor.cpp
-  real_robot/real_epuck_virtual_camrab_sensor.cpp
+  #  real_robot/real_epuck_omnidirectional_camera_sensor.cpp
+  #  real_robot/real_epuck_virtual_camrab_sensor.cpp
   # Actuators
   real_robot/real_epuck_wheels_actuator.cpp
   real_robot/real_epuck_base_leds_actuator.cpp
   real_robot/real_epuck_rgb_leds_actuator.cpp
   real_robot/real_epuck_range_and_bearing_actuator.cpp
-  real_robot/real_epuck_ircom_actuator.cpp
-  real_robot/real_epuck_virtual_camrab_actuator.cpp
+  real_robot/real_epuck_ircom_actuator.cpp)
+  #  real_robot/real_epuck_virtual_camrab_actuator.cpp
   # Vision
-  real_robot/vision/blob_detector.cpp
-  real_robot/vision/epuck_image_grabber.cpp
-  real_robot/vision/distance_converter.cpp
-  real_robot/vision/image.cpp)
+  #  real_robot/vision/blob_detector.cpp
+  #  real_robot/vision/epuck_image_grabber.cpp
+  #  real_robot/vision/distance_converter.cpp
+  #  real_robot/vision/image.cpp)
 
 #
 # Create e-puck library
@@ -103,14 +103,14 @@ install(TARGETS argos3plugin_${ARGOS_BUILD_FOR}_epuck
 #
 # Create configurator server
 #
-set(ARGOS3_SERVER_SOURCES_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION
-  ${ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION}
-  real_robot/vision/blob_detector.cpp
-  real_robot/vision/epuck_camera_configurator.cpp
-  real_robot/vision/epuck_image_grabber.cpp
-  real_robot/vision/distance_converter.cpp
-  real_robot/vision/image.cpp)
-add_executable(epuck_camera_configurator_server
-  ${ARGOS3_SERVER_SOURCES_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION})
-target_link_libraries(epuck_camera_configurator_server
-  argos3core_${ARGOS_BUILD_FOR})
+#set(ARGOS3_SERVER_SOURCES_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION
+#  ${ARGOS3_HEADERS_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION}
+#  real_robot/vision/blob_detector.cpp
+#  real_robot/vision/epuck_camera_configurator.cpp
+#  real_robot/vision/epuck_image_grabber.cpp
+#  real_robot/vision/distance_converter.cpp
+#  real_robot/vision/image.cpp)
+#add_executable(epuck_camera_configurator_server
+#  ${ARGOS3_SERVER_SOURCES_PLUGINS_ROBOTS_EPUCK_REALROBOT_VISION})
+#target_link_libraries(epuck_camera_configurator_server
+#  argos3core_${ARGOS_BUILD_FOR})
